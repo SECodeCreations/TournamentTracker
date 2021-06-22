56b617f19ea868fa1dc2e3aefed9ff8734d24378
SECodeCreations <s.ellison87@gmail.com>
2021-06-22 11:11:46 +0100

    Lesson 23:  * Added SQL storage for scores and winning team. * Added winners which are automatically added to the next round. * Next rounds are automatically populated.
    * Added Text File storage solution for the Tournament Viewer Window.

--- TrackerLibrary/DataAccess/IDataConnection.cs
+++ TrackerLibrary/DataAccess/IDataConnection.cs
@@ -14,6 +14,8 @@ namespace TrackerLibrary.DataAccess
         TeamModel CreateTeam(TeamModel model);
         void CreateTournament(TournamentModel model);
 
+        void UpdateMatchup(MatchupModel model);
+
         List<TeamModel> GetTeam_All();
         List<PersonModel> GetPerson_All();
         List<TournamentModel> GetTournament_All();
